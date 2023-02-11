0b997f72fa904b37621f5f90673ad008c363ff30
victornascimento <victooyooth@gmail.com>
2023-02-10 22:03:23 -0300

    sugestoes

---
+++ src/Theme.jsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles'
+
+export const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#895370',
+    },
+    secondary: {
+      main: '#eee0ea',
+    },
+  },
+})
